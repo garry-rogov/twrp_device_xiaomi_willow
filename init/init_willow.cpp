9be3dcf02a7d654c9ecb2bdc9e805d07faca3284
garry-rogov <garry.rogov@gmail.com>
2022-03-15 13:15:41 +0300

    fix init

--- init/init_willow.cpp
+++ init/init_willow.cpp
@@ -1,114 +1,62 @@
 /*
- * Copyright (C) 2020 Paranoid Android
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+   Copyright (C) 2017-2022 The Android Open Source Project
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <cstdlib>
-#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
-#include <sys/_system_properties.h>
-#include <sys/sysinfo.h>
-
+#include <unistd.h>
+#include <fcntl.h>
+#include <android-base/logging.h>
 #include <android-base/properties.h>
+
 #include "property_service.h"
-#include "vendor_init.h"
+#include "log.h"
 
-using android::base::GetProperty;
-using std::string;
+namespace android {
+namespace init {
 
-void property_override(string prop, string value)
-{
-    auto pi = (prop_info*) __system_property_find(prop.c_str());
+void load_properties(const char *model) {
+    property_set("ro.product.name", model);
+    property_set("ro.build.product", model);
+    property_set("ro.product.device", model);
+}
 
-    if (pi != nullptr)
-        __system_property_update(pi, value.c_str(), value.size());
+void vendor_load_properties() {
+    property_set("ro.bootimage.build.date.utc", "1546335651");
+    property_set("ro.build.date.utc", "1546335651");
+    std::string device_region = android::base::GetProperty("ro.boot.hwc", "");
+    std::string device_hwversion = android::base::GetProperty("ro.boot.hwversion", "");
+    if (device_region == "Global_B" && (device_hwversion == "18.31.0" || device_hwversion == "18.39.0" || device_hwversion == "19.39.0"))
+    {
+        load_properties("willow");
+    }
     else
-        __system_property_add(prop.c_str(), prop.size(), value.c_str(), value.size());
+    {
+        load_properties("ginkgo");
+    }
 }
 
-void vendor_load_properties()
-{
-    string device, model;
-
-    string region = GetProperty("ro.boot.hwc", "");
-    string hwversion = GetProperty("ro.boot.hwversion", "");
-
-    if (region == "Global_B" && (hwversion == "18.31.0" ||
-        hwversion == "18.39.0" || hwversion == "19.39.0")) {
-        device = "willow";
-        model = "Redmi Note 8T";
-    } else {
-        device = "ginkgo";
-        model = "Redmi Note 8";
-    }
-
-    // Override all partitions' props
-    string prop_partitions[] = { "", "odm.", "product.", "system.",
-					"system_ext.", "bootimage.", "vendor." };
-
-    for (const string &prop : prop_partitions) {
-        property_override(string("ro.product.") + prop + string("name"), device);
-        property_override(string("ro.product.") + prop + string("device"), device);
-        property_override(string("ro.product.") + prop + string("model"), model);
-        property_override(string("ro.") + prop + string("build.product"), device);
-    }
-
-    // Set hardware revision
-    property_override("ro.boot.hardware.revision", hwversion);
-
-    // Set hardware SKU prop
-    property_override("ro.boot.product.hardware.sku", device);
-
-    // Set camera model for EXIF data
-    property_override("persist.vendor.camera.model", model);
-
-    // Set dalvik heap configuration
-    string heapstartsize, heapgrowthlimit, heapsize, heapminfree,
-			heapmaxfree, heaptargetutilization;
-
-    struct sysinfo sys;
-    sysinfo(&sys);
-
-    if (sys.totalram > 5072ull * 1024 * 1024) {
-        // from - phone-xhdpi-6144-dalvik-heap.mk
-        heapstartsize = "16m";
-        heapgrowthlimit = "256m";
-        heapsize = "512m";
-        heaptargetutilization = "0.5";
-        heapminfree = "8m";
-        heapmaxfree = "32m";
-    } else if (sys.totalram > 3072ull * 1024 * 1024) {
-        // from - phone-xhdpi-4096-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "192m";
-        heapsize = "512m";
-        heaptargetutilization = "0.6";
-        heapminfree = "8m";
-        heapmaxfree = "16m";
-    } else {
-        // from - phone-xhdpi-2048-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "192m";
-        heapsize = "512m";
-        heaptargetutilization = "0.75";
-        heapminfree = "512k";
-        heapmaxfree = "8m";
-    }
-
-    property_override("dalvik.vm.heapstartsize", heapstartsize);
-    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_override("dalvik.vm.heapsize", heapsize);
-    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
-    property_override("dalvik.vm.heapminfree", heapminfree);
-    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
-}
+}  // namespace init
+}  // namespace android
